9c0f217edf6678f54f2cb2daaf163f29456f4be4
TheRealJD3 <jasonshawndsouza@gmail.com>
2018-06-06 01:16:51 +0400

    works?

--- dbconfig.php
+++ dbconfig.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("x3ztd854gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "xa8gfb64lxld077o", "pvx0im66k1csu737 ", "sgqp3pq60ksm4vdk");
+$mysqli = new mysqli("x3ztd854gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "xa8gfb64lxld077o", "pvx0im66k1csu737", "sgqp3pq60ksm4vdk");
 
 /* check connection */
 if (mysqli_connect_errno()) {
